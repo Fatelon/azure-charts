f1422eda0a1ee4ae993320e0fa8454e4de6381f7
Fatelon (Sergei) Petrov (UI) <sergei.petrov@juriba.com>
2021-04-21 16:32:05 +0300

    to ng 11

--- src/app/simple-chart/simple-chart.component.spec.ts
+++ src/app/simple-chart/simple-chart.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -9,7 +9,7 @@ describe('SimpleChartComponent', () => {
   let component: SimpleChartComponent;
   let fixture: ComponentFixture<SimpleChartComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SimpleChartComponent ]
     })
